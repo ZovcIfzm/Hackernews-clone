21614fff697cb413cf166728516435d0ed2695bf
Alex Ruan <alexruan2222@gmail.com>
2020-09-05 17:14:33 -0400

    authentication

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,25 +1,16 @@
 const { GraphQLServer } = require("graphql-yoga");
 const { PrismaClient } = require("@prisma/client");
 
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+
 const resolvers = {
-  Query: {
-    info: () => null,
-    feed: async (parent, args, context) => {
-      return context.prisma.link.findMany();
-    }
-  },
-  Mutation: {
-    // 2
-    post: (parent, args, context, info) => {
-      const newLink = context.prisma.link.create({
-        data: {
-          url: args.url,
-          description: args.description
-        }
-      });
-      return newLink;
-    }
-  }
+  Query,
+  Mutation,
+  User,
+  Link
 };
 
 const prisma = new PrismaClient();
@@ -27,8 +18,12 @@ const prisma = new PrismaClient();
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers,
-  context: {
-    prisma
+
+  context: (request) => {
+    return {
+      ...request,
+      prisma
+    };
   }
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
